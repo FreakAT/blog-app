520f68d7976c965a1eacaf059e718f5a6ed8481b
Hariom <hariom12062000@gmail.com>
2024-04-20 12:59:58 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignOut from "./pages/SignOut";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
 	return (
-		<>
-			<h1>My Blog</h1>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-out" element={<SignOut />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
