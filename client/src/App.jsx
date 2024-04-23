61d351a4e4b5bcdf681575f36a693f89980913e0
Hariom <hariom12062000@gmail.com>
2024-04-23 13:25:05 +0530

    created Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import SignOut from "./pages/SignOut";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/sign-in" element={<SignIn />} />
 				<Route path="/sign-out" element={<SignOut />} />
 				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/projects" element={<Projects />} />
 			</Routes>
 		</BrowserRouter>
 	);
