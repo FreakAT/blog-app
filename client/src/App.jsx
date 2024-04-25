0a6bd3217f7febf512fb0d1f6f58a41925daf948
Hariom <hariom12062000@gmail.com>
2024-04-25 22:45:04 +0530

    added SignUp UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
@@ -15,7 +15,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/sign-in" element={<SignIn />} />
-				<Route path="/sign-out" element={<SignOut />} />
+				<Route path="/sign-up" element={<SignUp />} />
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/projects" element={<Projects />} />
 			</Routes>
